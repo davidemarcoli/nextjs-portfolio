d5624596e371cc998ea783d2c830a2d168b72c03
davidemarcoli <davide@marcoli.ch>
2023-08-27 17:22:55 +0200

    Add a few mobile bugfixes (see https://github.com/chronark/chronark.com/pull/23)

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
 			<nav className="my-16 animate-fade-in">
-				<ul className="flex items-center justify-center gap-4">
+				<ul className="flex flex-wrap items-center justify-center gap-4">
 					{navigation.map((item) => (
 						<li>
 							<Link
@@ -40,7 +40,7 @@ export default function Home() {
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-sm text-zinc-500 ">
+				<h2 className="text-sm text-zinc-500 mx-6">
 					Hi, my name is Davide, I'm a swiss developer based near Zurich.
 					I'm currently working at{" "}
 					<Link
@@ -51,7 +51,7 @@ export default function Home() {
 						SIX
 					</Link>
 
-					<br />
+					<wbr />
 					and going to school at the{" "}
 					<Link
 						target="_blank"
